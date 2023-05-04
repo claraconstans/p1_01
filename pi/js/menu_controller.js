c32c7a99d542a2e7eb27cae10463b13fb57afd53
claraconstans <u1979264@campus.udg.edu>
2023-05-04 21:35:46 +0200

    errors solucionats menu vue

--- pi/js/menu_controller.js
+++ pi/js/menu_controller.js
@@ -5,31 +5,26 @@ var menu = new Vue({
 			sessionStorage.clear();
 			name = prompt("User name");
 			sessionStorage.setItem("username", name);
-			loadpage("./game.html");
+			loadpage("./html/game.html");
 		},
-
 		phaser_game_mode1(){
 			sessionStorage.clear();
 			name = prompt("User name");
 			sessionStorage.setItem("username", name);
 			loadpage("./phasergame_mode1.html");
 		},
-
 		phaser_game_mode2(){
 			sessionStorage.clear();
 			name = prompt("User name");
 			sessionStorage.setItem("username", name);
 			loadpage("./phasergame_mode2.html");
 		},
-
 		puntuacio(){
 			loadpage("./html/puntuacio.html");
 		},
-
 		jugar(){
 			loadpage("./html/jugar.html");
 		},
-
 		exit(){
 			if (name != ""){
 				alert("Leaving " + name + "'s game");
@@ -38,11 +33,9 @@ var menu = new Vue({
 			localStorage.clear();
 			loadpage("../index.html");
 		},
-
 		options(){
 			loadpage("./html/options.html");
 		},
-
 		load(){
 			loadpage("./html/load.html");
 		}
