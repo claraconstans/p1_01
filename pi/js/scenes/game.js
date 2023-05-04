ffd4008ef2d85065bf800b2c0118aa40c96f0ceb
claraconstans <u1979264@campus.udg.edu>
2023-05-04 14:20:47 +0200

    mostrar cartes quan es faci un error

--- pi/js/scenes/game.js
+++ pi/js/scenes/game.js
@@ -46,31 +46,19 @@ class GameScene extends Phaser.Scene {
 		arraycards.sort(function(){return Math.random() - 0.5});
 
 		var numArray=0;
-		for (var c = 0; c < numCartes; c++){
-			for (var f = 0; f < 2; f++){
+		for (let c = 0; c < numCartes; c++){
+			for (let f = 0; f < 2; f++){
 				this.add.image(this.cameras.main.centerX+125*c-96*numCartes/2, this.cameras.main.centerY-128*numCartes/2+150*f, arraycards[numArray]);
 				numArray++;
 			}
 		}
 		this.cards = this.physics.add.staticGroup();
-		for (var c = 0; c < numCartes; c++){
-			for (var f = 0; f < 2; f++){
+		for (let c = 0; c < numCartes; c++){
+			for (let f = 0; f < 2; f++){
 				this.cards.create(this.cameras.main.centerX+125*c-96*numCartes/2, this.cameras.main.centerY-128*numCartes/2+150*f, 'back');
 			}
 		}
-
-		/*this.add.image(250, 300, arraycards[0]);
-		this.add.image(350, 300, arraycards[1]);
-		this.add.image(450, 300, arraycards[2]);
-		this.add.image(550, 300, arraycards[3]);
-		
-		this.cards = this.physics.add.staticGroup();
-		
-		this.cards.create(250, 300, 'back');
-		this.cards.create(350, 300, 'back');
-		this.cards.create(450, 300, 'back');
-		this.cards.create(550, 300, 'back');*/
-		
+				
 		let i = 0;
 		this.cards.children.iterate((card)=>{
 			card.card_id = arraycards[i];
@@ -83,6 +71,25 @@ class GameScene extends Phaser.Scene {
 						this.score -= puntsRestar;
 						this.firstClick.enableBody(false, 0, 0, true, true);
 						card.enableBody(false, 0, 0, true, true);
+						
+						var error = [];
+						let aux = 0;
+
+						for(let i = 0; i < numCartes*2; i++){
+							for (let c = 0; c < numCartes; c++){
+								for (let f = 0; f < 2; f++){
+									let imatge = this.add.image(this.cameras.main.centerX+125*c-96*numCartes/2, this.cameras.main.centerY-128*numCartes/2+150*f, arraycards[aux]);
+									error.push(imatge);
+									aux++;
+								}
+							}
+						}
+
+						setTimeout(() =>{
+							for (let i = 0; i < numCartes*2; i++){
+								error[i].destroy();
+							}
+						},temps);
 
 						if (this.score <= 0){
 							alert("Game Over");
