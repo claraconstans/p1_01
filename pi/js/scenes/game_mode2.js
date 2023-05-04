c9498e0d73028d1cc559fb2fa737930534b25cd6
claraconstans <u1979264@campus.udg.edu>
2023-05-04 16:52:12 +0200

    funciona basic mode 2

--- pi/js/scenes/game_mode2.js
+++ pi/js/scenes/game_mode2.js
@@ -4,6 +4,7 @@ class GameScene extends Phaser.Scene {
         this.cards = null;
         this.firstClick = null;
         this.score = 100;
+        this.level = 1;
         this.correct = 0;
         this.player = "";
     }
@@ -22,10 +23,25 @@ class GameScene extends Phaser.Scene {
         let arraycards = ['cb', 'cb', 'co', 'co', 'sb', 'sb', 'so', 'so', 'tb', 'tb', 'to', 'to'];
 		this.cameras.main.setBackgroundColor(0x98b396);
         
-		var numCartes = 2;
-		var dificultat = null;
+        var json = sessionStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
+		var options_data = JSON.parse(json);
+		var numCartes = options_data.cards;
+		var dificultat = options_data.dificulty;
 		var temps = null;
 		var puntsRestar = null;
+
+        if (dificultat == "easy"){
+			temps = 2000;
+			puntsRestar = 5;
+		}
+		else if (dificultat == "normal"){
+			temps = 1000;
+			puntsRestar = 10;
+		}
+		else{
+			temps = 500;
+			puntsRestar = 20;
+		}
 		
 		arraycards = arraycards.slice(0, numCartes*2);
 		arraycards.sort(function(){return Math.random() - 0.5});
@@ -43,7 +59,71 @@ class GameScene extends Phaser.Scene {
 				this.cards.create(this.cameras.main.centerX+125*c-96*numCartes/2, this.cameras.main.centerY-128*numCartes/2+150*f, 'back');
 			}
 		}
-    }
+        let i = 0;
+        this.cards.children.iterate((card)=>{
+			card.card_id = arraycards[i];
+			i++;
+			card.setInteractive();
+			card.on('pointerup', () => {
+				card.disableBody(true,true);
+				if (this.firstClick){
+					if (this.firstClick.card_id !== card.card_id){
+						this.score -= puntsRestar;
+						this.firstClick.enableBody(false, 0, 0, true, true);
+						card.enableBody(false, 0, 0, true, true);
+						
+						var error = [];
+						let aux = 0;
+
+						for(let i = 0; i < numCartes*2; i++){
+							for (let c = 0; c < numCartes; c++){
+								for (let f = 0; f < 2; f++){
+									let imatge = this.add.image(this.cameras.main.centerX+125*c-96*numCartes/2, this.cameras.main.centerY-128*numCartes/2+150*f, arraycards[aux]);
+									error.push(imatge);
+									aux++;
+								}
+							}
+						}
+
+						setTimeout(() =>{
+							for (let i = 0; i < numCartes*2; i++){
+								error[i].destroy();
+							}
+						},temps);
+
+						if (this.score <= 0){
+							alert("Game Over");
+							loadpage("../");
+						}
+					}
+					else{
+						this.correct++;
+						if (this.correct >= numCartes){
+							this.level++;
+                            this.correct = 0;
+                            if (numCartes<6) numCartes++;
+                            if(dificultat=="easy"){
+                                dificultat = "normal";
+                            }
+                            else if (dificultat == "normal"){
+                                dificultat = "hard";
+                            }
+                            puntsRestar +=10;
+                            options_data.cards = numCartes;
+							options_data.dificulty = dificultat;
+							options_data.puntsPerd = puntsRestar;	
+							sessionStorage.setItem("config", JSON.stringify(options_data));
+                            this.scene.restart();
+						}
+					}
+					this.firstClick = null;
+				}
+				else{
+					this.firstClick = card;
+				}
+			}, card);
+		});
+	}
 
 	update (){	
     }
